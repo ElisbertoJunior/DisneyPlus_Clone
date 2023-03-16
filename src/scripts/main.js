1e3a789be7802e94b5298d8890863279903f2646
Elisberto Junior <elisberto_jr@hotmail.com>
2023-03-15 22:36:34 -0300

    add tab shows

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,15 +13,15 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-const showActiveList = (tab) => tab.classList.add("shows__list--is-active");
+const showActiveList = tab => tab.classList.add("shows__list--is-active");
 
-const showSelectedButton = (btn) =>
+const showSelectedButton = btn =>
   btn.target.classList.add("shows__tabs__button--is-active");
 
 const removeActiveButton = () => {
   const buttons = document.querySelectorAll("[data-tab-button]");
 
-  buttons.forEach((button) => {
+  buttons.forEach(button => {
     button.classList.remove("shows__tabs__button--is-active");
   });
 };
@@ -29,7 +29,7 @@ const removeActiveButton = () => {
 const hideAllTabs = () => {
   const tabsContainer = document.querySelectorAll("[data-tab-id]");
 
-  tabsContainer.forEach((tab) => {
+  tabsContainer.forEach(tab => {
     tab.classList.remove("shows__list--is-active");
   });
 };
